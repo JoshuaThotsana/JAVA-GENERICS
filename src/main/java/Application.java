f98f6d283381d5181e35228e8e591b9b4753849e
Thotsana <thotsana.mabotsa@umuzi.org>
2020-02-13 14:00:49 +0200

    initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,42 @@
+/**
+ * A simple introduction to Java Generics.
+ *
+ * @author Joshua Thotsana Mabotsa.
+ */
+public class Application{
+
+    public static void main(String[] args) {
+
+        Integer[] intArray = {12,324,6,7900};
+
+        String[] stringArray = {"I","Love","To","Code"};
+
+        printArrayContent(intArray);
+        System.out.println();
+
+        printArrayContent(stringArray);
+        System.out.println();
+
+        GenericsClass<String> stringInstance = new GenericsClass<>();
+        stringInstance.set("Test");
+
+        stringInstance.get(); // print out Test
+
+        GenericsClass<Integer> integerInstance = new GenericsClass<>();
+        integerInstance.set(1000);
+
+        integerInstance.get(); // print out 1000
+    }
+
+    /**
+     * A function that accepts one parameter and it is able to accept an integer array or string array and prints out
+     * the contents of that array.
+     * @param array     the array argument.
+     * @param <T>       the Generic object parameter.
+     */
+    public static  <T> void printArrayContent(T[] array) {
+        for (T t : array) {
+            System.out.println(t);
+        }
+    }
+}
---
+++ src/main/java/GenericsClass.java
@@ -0,0 +1,20 @@
+public class GenericsClass <T> {
+    // Instance variables;
+    private T variable;
+
+    /**
+     * Get the content of the array and prints them on the console.
+     */
+    public void get() {
+        System.out.println(variable);
+    }
+
+    /**
+     * This is a setter for the private instance variable.
+     * @param variable the private variable to be set.
+     */
+    public void set(T variable) {
+        this.variable = variable;
+    }
+
+}
